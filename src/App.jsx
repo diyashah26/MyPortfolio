8967a40ffd7d42289bcc8f68549d013c11ecaf5e
diyashah26 <diyashah2628@gmail.com>
2025-03-24 23:13:04 +0530

    netlify push - 8

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,7 @@ import Layout from "./Layout"; // You forgot to import Layout in App.jsx
 function App() {
   return (
     <div>
-      {/* Dummy form for Netlify detection */}
-      <form name="feedback" netlify netlify-honeypot="bot-field" hidden>
-        <input type="text" name="name" />
-        <input type="email" name="email" />
-        <textarea name="message"></textarea>
-      </form>
-
-      {/* Main Layout with your actual app */}
-      <Layout />
+      
     </div>
   );
 }
